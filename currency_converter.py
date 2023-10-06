8f7c6a365101cd08622485904bb9f51e5bc23061
Jacob Olness <jolness1@gmail.com>
2023-10-06 14:14:30 -0600

    fixes to error handling

--- currency_converter.py
+++ currency_converter.py
@@ -40,22 +40,31 @@ CURRENCY_CODES = {
 
 
 def convert_currency(amount, start_currency, end_currency):
-    if start_currency not in CURRENCY_CODES:
-        raise ValueError(f"Invalid start currency: {start_currency}")
-    if end_currency not in CURRENCY_CODES:
-        raise ValueError(f"Invalid end currency: {end_currency}")
-    url = "https://api.freecurrencyapi.com/v1/latest?apikey={apikey}&base_currency={start_currency}&currencies={end_currency}"
-    response = requests.get(
-        url.format(apikey="fca_live_Rmp5gWHFF5juckEH9pHQrokJv83Cm2YYBhXLyUyv", start_currency=start_currency,
-                   end_currency=end_currency))
-    data = response.json()
+    try:
+        if start_currency not in CURRENCY_CODES:
+            raise ValueError(f"Invalid start currency: {start_currency}")
+        if end_currency not in CURRENCY_CODES:
+            raise ValueError(f"Invalid end currency: {end_currency}")
 
-    if response.status_code != 200:
-        raise Exception(f"Failed to convert currency: {response.status_code} {response.reason}")
+        url = "https://api.freecurrencyapi.com/v1/latest?apikey={apikey}&base_currency={start_currency}&currencies={end_currency}"
+        response = requests.get(
+            url.format(apikey="fca_live_Rmp5gWHFF5juckEH9pHQrokJv83Cm2YYBhXLyUyv", start_currency=start_currency,
+                       end_currency=end_currency))
+        data = response.json()
 
-    conversion_rate = data["data"][end_currency]
-    converted_amount = amount * conversion_rate
-    return converted_amount
+        if response.status_code != 200:
+            raise Exception(f"Failed to convert currency: {response.status_code} {response.reason}")
+
+        conversion_rate = data["data"][end_currency]
+        converted_amount = amount * conversion_rate
+        return converted_amount
+
+    except ValueError as ve:
+        print(f"ValueError: {ve}")
+        return None
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        raise
 
 
 def list_currencies():
